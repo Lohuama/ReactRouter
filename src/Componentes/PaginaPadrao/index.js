f69947ca33456276a84e23b1bfdbc638ffaa510f
Lohuama <limalorruama98@gmail.com>
2024-02-06 11:17:13 -0300

    versao final

--- src/Componentes/PaginaPadrao/index.js
+++ src/Componentes/PaginaPadrao/index.js
@@ -2,12 +2,13 @@ import styles from './PaginaPadrao.module.css'
 import Banner from '../Banner'
 import { Outlet } from 'react-router-dom'
 
-export default function PaginaPadrao(){
+export default function PaginaPadrao({ children }){
     return (
         <main>
             <Banner />
 
             <Outlet />
+            {children}
         </main>
     )
 }
--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,28 +1,30 @@
-import { useParams } from 'react-router-dom';
-import ReactMarkdown from 'react-markdown'
-import posts from '../../json/posts.json'
-import PostModelo from '../../Componentes/PostModelo'
-import './Post.css'
-export default function Post(){
-    const parametros = useParams();
-    const post = posts.find((post) => {
-        return post.id === Number(parametros.id);
-    });
+import { useParams } from "react-router-dom";
+import ReactMarkdown from "react-markdown";
+import posts from "../../json/posts.json";
+import PostModelo from "../../Componentes/PostModelo";
+import NaoEncontrada from "../NaoEncontrada";
+import "./Post.css";
+import PaginaPadrao from "../../Componentes/PaginaPadrao";
+export default function Post() {
+  const parametros = useParams();
+  const post = posts.find((post) => {
+    return post.id === Number(parametros.id);
+  });
 
-    if(!post){
-        return <h1>Post não encontrado...</h1>
-    }
+  if (!post) {
+    return <NaoEncontrada />;
+  }
 
-    return (
-        <PostModelo
-            FotoCapa={`../../assets/posts/${post.id}/capa.png`}
-            título={post.titulo}
-        >
-            <div className='post-markdown-container'>
-                <ReactMarkdown>
-                    {post.texto}
-                </ReactMarkdown>
-            </div>
-        </PostModelo>
-    )
+  return (
+    <PaginaPadrao>
+      <PostModelo
+        FotoCapa={`../../assets/posts/${post.id}/capa.png`}
+        título={post.titulo}
+      >
+        <div className="post-markdown-container">
+          <ReactMarkdown>{post.texto}</ReactMarkdown>
+        </div>
+      </PostModelo>
+    </PaginaPadrao>
+  );
 }
